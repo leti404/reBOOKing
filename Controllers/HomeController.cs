176fb6abacacc754080787c8e8cdb0d63882bb10
LETICIA ROCÍO PASSER <48386947@ort>
2024-11-04 08:06:56 -0300

    cambios navbar

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -55,11 +55,11 @@ public class HomeController : Controller
     {
         return View();
     }
-    public IActionResult Donar()
+    
+    public IActionResult Carrito()
     {
         return View();
     }
-    
     public IActionResult CrearPublicacion()
     {
         return View();
